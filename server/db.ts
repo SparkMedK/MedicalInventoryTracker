6b4ce23b7b0baca0f4157042e736b2b8b75a087b
SparkMedK <medkerkeni.dev@gmail.com>
2025-06-30 13:48:21 +0000

    feat add .env

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
+// Ensure the WebSocket constructor is set for Neon
+
+console.log("DATABASE_URL loaded:", process.env.DATABASE_URL);
 
 neonConfig.webSocketConstructor = ws;
-
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
